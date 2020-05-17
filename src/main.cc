a1fc5664d6f677754dee4317c754ada174dadbab
Fahad Riaz <fahadbr@gmail.com>
2020-05-17 15:47:00 -0400

    initial commit

---
+++ src/main.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ncurses.h>
+
+int main() {
+	//std::cout << "hello world";
+
+	initscr(); // start curses mode
+	raw(); // line buffering disabled
+	keypad(stdscr, TRUE); // we get F1 F2 etc
+	noecho(); // dont echo() while we do getch
+
+	printw("Type any character to see it in bold\n"); // print hello world
+	int ch = getch();
+	if (ch == KEY_F(1))
+		printw("F1 key pressed");
+	else
+	{
+		printw("The pressed key is ");
+		attron(A_BOLD);
+		printw("%c", ch);
+		attroff(A_BOLD);
+	}
+
+	refresh(); //print it on to the real screen
+	getch(); //wait for user input
+	endwin(); //end curses mode
+
+	return 0;
+}
